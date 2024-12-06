8000e376fe9830166ed29efdc46bd981bf71d0e6
JMS55 <47158642+JMS55@users.noreply.github.com>
2024-12-06 14:59:55 -0800

    WIP: Implement structure of  command_encoder_transition_resources

--- wgpu-core/src/command/transition_resources.rs
+++ wgpu-core/src/command/transition_resources.rs
@@ -1,4 +1,9 @@
-use crate::{global::Global, id::CommandEncoderId};
+use thiserror::Error;
+
+use crate::{
+    command::CommandBuffer, device::DeviceError, global::Global, id::CommandEncoderId,
+    track::ResourceUsageCompatibilityError,
+};
 
 use super::CommandEncoderError;
 
@@ -8,9 +13,52 @@ impl Global {
         command_encoder_id: CommandEncoderId,
         buffer_transitions: &[()],
         texture_transitions: &[()],
-    ) -> Result<(), CommandEncoderError> {
+    ) -> Result<(), TransitionResourcesError> {
         profiling::scope!("CommandEncoder::transition_resources");
+
+        let hub = &self.hub;
+
+        let cmd_buf = hub
+            .command_buffers
+            .get(command_encoder_id.into_command_buffer_id());
+        let mut cmd_buf_data = cmd_buf.data.lock();
+        let mut cmd_buf_data_guard = cmd_buf_data.record()?;
+        let cmd_buf_data = &mut *cmd_buf_data_guard;
+
+        let device = &cmd_buf.device;
+        let snatch_guard = &device.snatchable_lock.read();
+
+        let mut usage_scope = device.new_usage_scope();
+
+        for buffer_transition in buffer_transitions {
+            usage_scope.buffers.merge_single(todo!(), todo!())?;
+        }
+
+        for texture_transition in texture_transitions {
+            unsafe { usage_scope.textures.merge_single(todo!(), todo!(), todo!()) }?;
+        }
+
+        let cmd_buf_raw = cmd_buf_data.encoder.open(device)?;
+        CommandBuffer::insert_barriers_from_scope(
+            cmd_buf_raw,
+            &mut cmd_buf_data.trackers,
+            &usage_scope,
+            snatch_guard,
+        );
+        cmd_buf_data_guard.mark_successful();
 
         Ok(())
     }
+}
+
+/// Error encountered while attempting to perform [`Global::command_encoder_transition_resources`].
+#[derive(Clone, Debug, Error)]
+#[non_exhaustive]
+pub enum TransitionResourcesError {
+    #[error(transparent)]
+    Device(#[from] DeviceError),
+    #[error(transparent)]
+    Encoder(#[from] CommandEncoderError),
+    #[error(transparent)]
+    ResourceUsage(#[from] ResourceUsageCompatibilityError),
 }
