dacb25933acfc27c6a541ca761f8a3e5b6049ad3
JMS55 <47158642+JMS55@users.noreply.github.com>
2025-01-07 19:27:07 -0500

    Fix merge

--- wgpu-core/src/command/transition_resources.rs
+++ wgpu-core/src/command/transition_resources.rs
@@ -59,7 +59,7 @@ impl Global {
         }
 
         // Record any needed barriers based on tracker data
-        let cmd_buf_raw = cmd_buf_data.encoder.open(device)?;
+        let cmd_buf_raw = cmd_buf_data.encoder.open()?;
         CommandBuffer::insert_barriers_from_scope(
             cmd_buf_raw,
             &mut cmd_buf_data.trackers,
